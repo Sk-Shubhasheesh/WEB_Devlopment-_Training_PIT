69afdbb6a6096ca8aaccf56875e55b8785a4c3f6
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-06-17 20:17:42 +0530

    Data Type in Js

---
+++ 3. JavaScript/3.1 Data Types/first.js
@@ -0,0 +1,73 @@
+/*
+There are two types of data in Js:
+1. Primitive Data Types:
+    Number
+    boolean
+    string
+    null
+    undefind
+    Symbol
+    bigint
+    
+2. Non Primitive Data Type
+   Array
+   Object
+   function
+
+*/
+
+
+
+
+// Number
+let account_blance = 20;
+console.log(account_blance);
+console.log(typeof account_blance);
+
+// String
+let str = "shubhasheesh is a boy he know how to use the zoom"
+console.log(str);
+console.log(typeof str);
+let comment = 'Hello Ji to kaise hai app sb';
+console.log(comment);
+
+// Boolean
+let statement = false;
+console.log(statement);
+console.log(typeof statement);
+
+
+// Undefined - not fill value only create variable
+let account;
+console.log(account);
+console.log(typeof account);
+
+// Null
+let balance = null;
+console.log(null);
+console.log(typeof balance); // object data type😊
+
+//BigInt
+let a = 478549903024943555555555554443566665n;
+console.log(a);
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+// Symbol
+let mySymbol = Symbol();
+console.log(mySymbol);
+
+let mySymbolWithDescription = Symbol("my description");
+console.log(mySymbolWithDescription);
+
+
+
+
+
+
+
+
+
+
+
