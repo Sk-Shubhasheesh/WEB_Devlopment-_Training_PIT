b013f5d9ec4beeb5b94adcdd5cd01fb0ecca6822
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-06-19 12:27:39 +0530

    Basic TypeCasting in Js

---
+++ 3. JavaScript/3.1 Data Types/third.js
@@ -0,0 +1,45 @@
+// TypeCasting
+
+
+// String to Number Conversion
+let account_balance = "100";
+let num  = Number(account_balance);
+console.log(typeof num);
+
+// Boolean to Number Conversion
+let x = true;
+console.log(Number(x));
+
+
+// Interesting Case
+let account  ="100xs"
+console.log(Number(account));
+
+
+// null to Number Conversion  
+let x1= null;
+console.log(Number(x1));
+
+// Undefined to Number Conversion
+let x2;
+console.log(Number(x2));
+
+// Number to String Conversion
+let ab = 20;
+console.log(String(ab));
+
+// Boolean to String
+let ax = true;
+console.log(String(ax));
+
+
+// Boolean Conversion
+let str = "Shubhu"
+console.log(Boolean(str));
+let str1 = ""
+console.log(Boolean(str1));
+let str2 = " "
+console.log(Boolean(str2));
+
+
+
