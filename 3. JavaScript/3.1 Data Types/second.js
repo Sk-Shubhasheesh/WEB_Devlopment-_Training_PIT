df451dbd6a1097b44552bb837aacb17291fbe0f7
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-06-19 12:01:12 +0530

    Non-Primitive Data Types

---
+++ 3. JavaScript/3.1 Data Types/second.js
@@ -0,0 +1,23 @@
+// Array
+let arr =[10,20,50,"rohit", "Sk"];
+console.log(arr);
+console.log(typeof arr);
+
+// Object
+let obj = {
+    user_name:"Shubhasheesh",
+    age: 25,
+    Exprience: "1 year At Wipro"
+}
+
+console.log(obj)
+console.log(typeof obj);
+;
+
+
+// Function
+let fun = function(){
+    console.log("Welcome to the world of JavScript");  
+}
+fun()
+console.log(typeof fun);
